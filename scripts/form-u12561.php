fbe10b0c3b74b6a97ffdde24aaacc5208d4f82a2
Darren White <drwhite@ootwcs.com>
2018-03-15 13:20:18 +0200

    com.ootwcs

--- scripts/form-u12561.php
+++ scripts/form-u12561.php
@@ -27,8 +27,8 @@ $form = array(
 		'unknown_method' => 'Unknown server request method'
 	),
 	'email' => array(
-		'from' => 'admin@ootwcs.com',
-		'to' => 'admin@ootwcs.com'
+		'from' => 'drwhite@ootwcs.com',
+		'to' => 'drwhite@ootwcs.com'
 	),
 	'fields' => array(
 		'custom_U12562' => array(
